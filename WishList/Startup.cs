29bd833b5ccf88a7723102cb1bdafd5086c8723f
Eric J Fisher <eric-fisher@pluralsight.com>
2020-01-29 17:17:44 -0500

    Update to .Net Core 3.1 including tasks, tests, and base code.

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using WishList.Data;
 
 namespace WishList
@@ -17,7 +18,7 @@ namespace WishList
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -28,7 +29,12 @@ namespace WishList
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseMvcWithDefaultRoute();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
         }
     }
 }
