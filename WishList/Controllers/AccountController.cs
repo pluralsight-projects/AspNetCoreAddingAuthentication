e25bb8153af75f65754e2a8022942e3a40e83720
Manoj1022770 <manojkumar.agasibagila@mindtree.com>
2020-11-22 15:27:24 +0530

    changes

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -88,7 +88,7 @@ namespace WishList.Controllers
         public IActionResult Logout()
         {
             _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Item");
+            return RedirectToAction("Index", "Home");
         }
     }
 }
